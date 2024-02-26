68791727c1d5d1e25a5752ea7f3ff0227714b001
arganaft <e1mar.arganaft@gmail.com>
2024-02-27 01:16:17 +0300

    2.3.7. Проект. Агрегатор Java вакансий 2.4. HabrCareerParse

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,46 +5,67 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
 
-    private static final String SOURCE_LINK = "https://career.habr.com";
-    public static final String PREFIX = "/vacancies?page=";
-    public static final String SUFFIX = "&q=Java%20developer&type=all";
+    private final String prefix = "/vacancies?page=";
+    private final String suffix = "&q=Java%20developer&type=all";
+    private final DateTimeParser dateTimeParser;
 
     public static void main(String[] args) throws IOException {
+        HabrCareerParse parser = new HabrCareerParse(new HabrCareerDateTimeParser());
+        parser.list("https://career.habr.com").forEach(System.out::println);
+    }
+
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
+    @Override
+    public List<Post> list(String link) throws IOException {
+        List<Post> posts = new LinkedList<>();
+
         int pageNumber = 1;
         Connection connection;
         Document document;
         while (pageNumber <= 5) {
-            String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
+            String fullLink = "%s%s%d%s".formatted(link, prefix, pageNumber, suffix);
             connection = Jsoup.connect(fullLink);
             document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> {
-                Element titleElement = row.select(".vacancy-card__title").first();
-
-                Element dateElement = row.select(".vacancy-card__date").first();
-                String date = dateElement.select("time").attr("datetime");
-
-                Element linkElement = titleElement.child(0);
-                String vacancyName = titleElement.text();
-                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                String description;
-                try {
-                    description = new HabrCareerParse().retrieveDescription(link);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.printf("%s %s date - %s%n", vacancyName, link, date);
-                System.out.println(description);
-            });
+            for (Element row : rows) {
+                posts.add(createPost(row, link));
+            }
             pageNumber++;
         }
-
+        return posts;
     }
+
+    private Post createPost(Element row, String link) {
+      Element titleElement = row.select(".vacancy-card__title").first();
+      Element dateElement = row.select(".vacancy-card__date").first();
+      String date = dateElement.select("time").attr("datetime");
+      Element linkElement = titleElement.child(0);
+      String vacancyName = titleElement.text();
+      String vacancyLink = String.format("%s%s", link, linkElement.attr("href"));
+      String description;
+      try {
+          description = retrieveDescription(vacancyLink);
+      } catch (IOException e) {
+          throw new RuntimeException(e);
+      }
+      System.out.printf("%s %s date - %s%n", vacancyName, vacancyLink, date);
+      System.out.println(description);
+      return new Post(vacancyName, vacancyLink, description, dateTimeParser.parse(date));
+    }
+
+
 
     private String retrieveDescription(String link) throws IOException {
         StringBuilder stringBuilder = new StringBuilder();
@@ -67,4 +88,6 @@ public class HabrCareerParse {
         }
         return stringBuilder;
     }
+
+
 }
